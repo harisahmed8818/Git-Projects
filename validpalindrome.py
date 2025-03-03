cc473e95fbe466ba653783f3aea33539c888c0f8
harisahmed8818 <haris.attique799@gmail.com>
2025-03-03 15:20:16 +0500

    Verifying if string is palindrome or not

---
+++ validpalindrome.py
@@ -0,0 +1,13 @@
+def is_palindrome(s):
+    s = s.lower()
+    new_s = ""
+    for char in s:
+        if char.isalnum():
+            new_s += char 
+
+    return new_s == new_s[::-1] 
+
+print(is_palindrome("A man, a plan, a canal: Panama"))
+print(is_palindrome("Raca a car"))
+print(is_palindrome("madam"))
+print(is_palindrome("Hello!"))
